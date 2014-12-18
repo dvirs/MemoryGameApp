fa60df9bdc0060586c01bed19de099c981ea115c
dvirs <dvir.sofer@walla.com>
2014-12-18 16:26:03 +0200

    card

--- app/src/main/java/com/example/admin/memorygame/classes/card.java
+++ app/src/main/java/com/example/admin/memorygame/classes/card.java
@@ -3,14 +3,25 @@ package com.example.admin.memorygame.classes;
 import android.content.Context;
 import android.widget.ImageButton;
 
-public class card extends ImageButton{
+public class Card extends ImageButton{
     private int id;
-//    private int side;
+    private int side;
 
-    public card(Context c,int id){
+    public Card(Context c, int id){
         super(c);
         this.id = id;
-//        this.side = 0;
+        this.side = 0;
     }
 
+    public int cardGetId(){
+        return this.id;
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
 }
